8dac45cfcfaf839d6b20d05ba9da26bdcc1346a2
thefamilygrog <theoam@hotmail.com>
2014-10-17 11:31:00 -0400

    Initial upload

---
+++ README.txt
@@ -0,0 +1,39 @@
+Mob Capture Containment And Release Ball (mccarball) mod for Minetest
+
+
+by thefamilygrog66
+
+Description:
+A spherical device that, when placed next to a mob, captures and contains said mob. The player can then pick up the ball and store it in their inventory until they wish to release the mob by placing the ball on the ground, on a wall, etc. By default, this mod works with PilzAdam's Simple Mobs (mobs) mod, though it will likely work with other mob mods (just adjust the array at the beginning of init.lua accordingly).
+
+Recipe:
+
+wool:orange | wool:orange | wool:orange
+---------------------------------------
+wool:black  | wool:white  | wool:black
+---------------------------------------
+wool:yellow | wool:yellow | wool:yellow
+
+Mod dependencies: mobs, default, wool
+
+License:
+Sourcecode: WTFPL (see below)
+Graphics: WTFPL (see below)
+Sounds: by Mike Koenig (CC BY 3.0)
+
+See also:
+http://minetest.net/
+
+         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ init.lua
@@ -0,0 +1,154 @@
+local mccarball_list = {
+	{ "MCCAR Ball (empty)", "empty"},
+	{ "MCCAR Ball with Sheep", "sheep"},
+	{ "MCCAR Ball with Rat", "rat"},
+	{ "MCCAR Ball with Stone Monster", "stone_monster"},
+	{ "MCCAR Ball with Sand Monster", "sand_monster"},
+	{ "MCCAR Ball with Dirt Monster", "dirt_monster"},
+	{ "MCCAR Ball with Tree Monster", "tree_monster"},
+	{ "MCCAR Ball with Oerkki", "oerkki"},
+	{ "MCCAR Ball with Dungeon Master", "dungeon_master"},
+}
+
+for i in ipairs(mccarball_list) do
+	local mccarballdesc = mccarball_list[i][1]
+	local mccarballtype = mccarball_list[i][2]
+	minetest.register_node("mccarball:"..mccarballtype.."ball", {
+	    description = mccarballdesc,
+	    drawtype = "nodebox",
+	    tiles = {
+	        "mccarball_top.png",
+	        "mccarball_bottom.png",
+	        "mccarball_side1.png",
+	        "mccarball_side2.png",
+	        "mccarball_side1.png",
+	        "mccarball_side2.png",
+	    },    
+	    paramtype = "light",
+		node_box = {
+	        type = "fixed",
+	        fixed = {        
+				-- 1st level
+	            { -0.125, -0.5,  -0.125,  0.1875, -0.4375,  0.0625, },
+	            { -0.0625, -0.5,  -0.1875,  0.125, -0.4375,  0.125, }, 
+	            -- 2nd level
+				{ -0.25, -0.4375,  -0.125,  0.3125, -0.375,  0.0625, },
+				{ -0.1875, -0.4375,  -0.1875,  0.25, -0.375,  0.125, },
+				{ -0.125, -0.4375,  -0.25,  0.1875, -0.375,  0.1875, },
+				{ -0.0625, -0.4375,  -0.3125,  0.125, -0.375,  0.25, },
+         		-- 3rd level
+				{ -0.3125, -0.375,  -0.125,  0.375, -0.3125,  0.0625, },
+				{ -0.25, -0.375,  -0.25,  0.3125, -0.3125,  0.1875, },
+				{ -0.1875, -0.375,  -0.3125,  0.25, -0.3125,  0.25, },
+				{ -0.0625, -0.375,  -0.375,  0.125, -0.3125,  0.3125, },
+         		--4th level
+ 				{ -0.3125, -0.3125,  -0.1875,  0.375, -0.25,  0.125, },
+				{ -0.25, -0.3125,  -0.3125,  0.3125, -0.25,  0.25, },
+				{ -0.125, -0.3125,  -0.375,  0.1875, -0.25,  0.3125, },
+        		--5th level
+				{ -0.375, -0.25,  -0.125,  0.4375, -0.1875,  0.0625, },
+				{ -0.3125, -0.25,  -0.25,  0.375, -0.1875,  0.1875, },
+				{ -0.25, -0.25,  -0.3125,  0.3125, -0.1875,  0.25, },
+				{ -0.1875, -0.25,  -0.375,  0.25, -0.1875,  0.3125, },
+				{ -0.0625, -0.25,  -0.4375,  0.125, -0.1875,  0.375, },
+        		--6th level
+				{ -0.375, -0.1875,  -0.1875,  0.4375, -0.125,  0.125, },
+				{ -0.3125, -0.1875,  -0.3125,  0.375, -0.125,  0.25, },
+				{ -0.25, -0.1875,  -0.375,  0.3125, -0.125,  0.3125, },
+				{ -0.125, -0.1875,  -0.4375,  0.1875, -0.125,  0.375, },
+        		--7th level
+				{ -0.375, -0.125,  -0.1875,  0.4375, -0.0625,  0.125, },
+				{ -0.3125, -0.125,  -0.3125,  0.375, -0.0625,  0.25, },
+				{ -0.25, -0.125,  -0.375,  0.3125, -0.0625,  0.3125, },
+				{ -0.125, -0.125,  -0.4375,  0.1875, -0.0625,  0.375, },        		
+        		--8th level
+				{ -0.375, -0.0625,  -0.1875,  0.4375, 0.0,  0.125, },
+				{ -0.3125, -0.0625,  -0.3125,  0.375, 0.0,  0.25, },
+				{ -0.25, -0.0625,  -0.375,  0.3125, 0.0,  0.3125, },
+				{ -0.125, -0.0625,  -0.4375,  0.1875, 0.0,  0.375, },
+				--9th level        		
+				{ -0.375, 0.0,  -0.125,  0.4375, 0.0625,  0.0625, },
+				{ -0.3125, 0.0,  -0.25,  0.375, 0.0625,  0.1875, },
+				{ -0.25, 0.0,  -0.3125,  0.3125, 0.0625,  0.25, },
+				{ -0.1875, 0.0,  -0.375,  0.25, 0.0625,  0.3125, },
+				{ -0.0625, 0.0,  -0.4375,  0.125, 0.0625,  0.375, },
+				--10th level
+ 				{ -0.3125, 0.0625,  -0.1875,  0.375, 0.125,  0.125, },
+				{ -0.25, 0.0625,  -0.3125,  0.3125, 0.125,  0.25, },
+				{ -0.125, 0.0625,  -0.375,  0.1875, 0.125,  0.3125, },
+				--11th level
+				{ -0.3125, 0.125,  -0.125,  0.375, 0.1875,  0.0625, },
+				{ -0.25, 0.125,  -0.25,  0.3125, 0.1875,  0.1875, },
+				{ -0.1875, 0.125,  -0.3125,  0.25, 0.1875,  0.25, },
+				{ -0.0625, 0.125,  -0.375,  0.125, 0.1875,  0.3125, },				
+				--12th level
+				{ -0.25, 0.1875,  -0.125,  0.3125, 0.25,  0.0625, },
+				{ -0.1875, 0.1875,  -0.1875,  0.25, 0.25,  0.125, },
+				{ -0.125, 0.1875,  -0.25,  0.1875, 0.25,  0.1875, },
+				{ -0.0625, 0.1875,  -0.3125,  0.125, 0.25,  0.25, },				
+				--13th level   
+				{ -0.125, 0.25,  -0.125,  0.1875, 0.3125,  0.0625, },
+	            { -0.0625, 0.25,  -0.1875,  0.125, 0.3125,  0.125, }, 
+	        }, 
+	    },
+	    sunlight_propagates = true,
+	    walkable = false,
+	    light_source = 14,
+	    selection_box = {
+	        type = "fixed",
+	        fixed = { -0.375, -0.5, -0.4375, 0.4375,0.3125, 0.375 },
+	    },
+		groups = { dig_immediate = 3 },
+		on_place = function(itemstack, placer, pointed_thing)
+			itemstack:take_item()
+			local locn = pointed_thing.above
+			minetest.env:add_node(locn, {name="mccarball:emptyball"})
+			if mccarballtype ~= "empty" then
+				-- drop mobs	
+				minetest.sound_play("mccarball_release", {
+					pos = {x=locn.x, y=locn.y, z=locn.z},
+					max_hear_distance = 10,
+				})
+				minetest.env:add_entity(locn, "mobs:"..mccarballtype)
+				local pbtype = mccarballtype:gsub("^%l", string.upper)
+				pbtype = pbtype:gsub('%W','')
+				minetest.chat_send_player(placer:get_player_name(),"Now releasing "..pbtype.."!")
+			else
+				-- catch nearest mob
+				local objs = minetest.get_objects_inside_radius({x=locn.x,y=locn.y,z=locn.z}, 3)
+				for k, obj in pairs(objs) do
+					if obj:get_luaentity() ~= nil then
+						local mobname = obj:get_luaentity().name
+						local enttype = string.sub(mobname, 1, 5)
+						if enttype == "mobs:" then
+							mobname = string.sub(mobname, 6)
+							minetest.sound_play("mccarball_capture", {
+								pos = {x=locn.x, y=locn.y, z=locn.z},
+								max_hear_distance = 10,
+							})
+							minetest.env:add_node(locn, {name="mccarball:"..mobname.."ball"})
+							obj:remove()
+							mobname = mobname:gsub("^%l", string.upper)
+							mobname = mobname:gsub('%W','')
+							minetest.chat_send_player(placer:get_player_name(),"You caught a "..mobname.."!")				
+							break
+						end					
+					end
+				end
+			end
+			return itemstack
+		end,						
+	})
+end
+
+minetest.register_craft({
+	output = "mccarball:emptyball",
+	recipe = {
+	{"wool:orange", "wool:orange", "wool:orange", },
+	{"wool:black", "wool:white", "wool:black", },
+	{"wool:yellow", "wool:yellow", "wool:yellow", }
+	}
+})
+
+
+
